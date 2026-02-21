3b7f1b22ec537d053f1c430086aa0a95924fe8d0
Ahmed Hossam <h.i.3rd.ar@gmail.com>
2026-02-21 22:16:55 +0200

    Rebuild Flutter web after API fix

--- middleware/adminOnly.js
+++ middleware/adminOnly.js
@@ -1,9 +1,9 @@
 function adminOnly(req, res, next) {
-  console.log("USER DATA:", req.user);
-
   if (!req.user || req.user.role !== "admin") {
     return res.status(403).json({ message: "Admin Access Required" });
   }
 
   next();
 }
+
+module.exports = adminOnly;
